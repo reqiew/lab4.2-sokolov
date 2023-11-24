fbe05160e835d3c75df56f6802708a472aca3854
dobry <dobry@DESKTOP-FBD3PAQ>
2023-11-24 16:04:49 +0300

    Добавьте файлы проекта.

---
+++ lab4.2-sokolov/Program.cs
@@ -0,0 +1,12 @@
+﻿int[,] mas = new int[5, 5];
+Random r = new Random();
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        mas[i, j] = r.Next(-9, 9);
+        if (mas[i, j] < 0) mas[i, j] = 0;
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
